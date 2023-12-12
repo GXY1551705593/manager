71a9b45f0c49a28bef626707a804622aa5c8e89a
gxy <1551705593@qq.com>
2023-12-12 23:30:06 +0800

    添加更新文章数据接口，但是接口返回null，没有调用成功

--- manager-server/routes/article.js
+++ manager-server/routes/article.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 router.prefix('/article');
 const Article = require("../models/articleSchema");
+const mongoose = require("mongoose");
 
 // 添加文章
 router.post("/add",async function(ctx,next){
@@ -19,5 +20,18 @@ router.post("/find",async function(ctx,next){
     };
 })
 
+// 更新文章数据
+router.post('/update',async function(ctx){
+    const {id,content,title,createTime} = ctx.request.body;
+    console.log('ctx.request.body ==',ctx.request.body);
+    const res = await Article.findByIdAndUpdate(id,{content,title,createTime});
+    console.log('res===',res)
+    if (res) {
+        ctx.response.body = {
+            status: 200,
+            data: res
+        }
+    }
+})
 
 module.exports = router
